bc2bb1835798e6ddcb39338388ecf74e4f4133bf
ye <solanoymf@gmail.com>
2025-04-16 14:49:39 +0200

    doing twig templates

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # peludor
 peludor website - about animals
+
+
+# Use composer for install dependencies
+composer intall
+
+## Run php server
+php -S localhost:8000
+
+url -> localhost:8080
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
+$twig = new \Twig\Environment($loader);
+
+$route = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+
+$routes = [
+    '/' => 'index.twig',
+    '/about' => 'about.twig',
+    '/errorPage' => 'errorPage.twig',
+    '/login' => 'login.twig',
+    '/magazine' => 'magazine.twig',
+    '/obituary' => 'obituary.twig'
+];
+
+if (array_key_exists($route, $routes)) {
+    echo $twig->render($routes[$route]);
+} else {
+    echo $twig->render('errorPage.twig', []);
+}
