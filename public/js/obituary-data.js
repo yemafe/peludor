a7e757ff03240a20e599bfbf5e0a918c47284342
ye <solanoymf@gmail.com>
2025-06-13 17:24:50 +0200

    some changes js

--- public/js/obituary-data.js
+++ public/js/obituary-data.js
@@ -3,33 +3,14 @@ const breedsData = {
     gato: ["Persa", "Siamés", "Maine Coon", "Bengala", "Sphynx", "British Shorthair", "Ragdoll", "Noruego de Bosque", "Exótico", "Otro"]
 };
 
-const citiesByCountry = {
-    España: ["Madrid","Barcelona","Valencia","Sevilla","Zaragoza","Málaga","Murcia","Palma","Bilbao","Alicante"],
-    México: ["CDMX","Guadalajara","Monterrey","Puebla","Tijuana","Mérida","Cancún","Querétaro","Chihuahua","Oaxaca"],
-    Colombia: ["Bogotá","Medellín","Cali","Barranquilla","Cartagena","Cúcuta","Bucaramanga","Pereira","Santa Marta","Manizales"]
-};
-
 document.addEventListener("DOMContentLoaded",()=>{
-    const type = document.getElementById("type");
+    const type = document.getElementById("petType");
     const breed = document.getElementById("breed");
     const customBreedCont = document.getElementById("custom-breed-container");
+    const breedCont = document.getElementById("breed-container");
     const mixed = document.getElementById("mixedBreed");
-    const country = document.getElementById("country");
-    const city = document.getElementById("city");
     const customTypeCont = document.getElementById("custom-type-container");
 
-    // Pueblos
-    Object.keys(citiesByCountry).forEach(c=>{
-        country.innerHTML += `<option>${c}</option>`;
-    });
-    country.addEventListener("change",()=>{
-        city.innerHTML = `<option disabled selected>Selecciona ciudad</option>`;
-        (citiesByCountry[country.value]||[]).forEach(ci=>{
-            city.innerHTML += `<option>${ci}</option>`;
-        });
-    });
-
-    // Raza y tipo
     type.addEventListener("change",()=>{
         const sel=type.value;
         customTypeCont.classList.toggle("d-none",sel!=="otro");
@@ -37,6 +18,7 @@ document.addEventListener("DOMContentLoaded",()=>{
         if(sel==="perro"||sel==="gato"){
             breedsData[sel].forEach(r=>breed.innerHTML+=`<option>${r}</option>`);
         }
+        breedCont.classList.toggle("d-none",sel=="otro");
     });
     breed.addEventListener("change",()=>{
         customBreedCont.classList.toggle("d-none",breed.value!=="Otro");
@@ -48,7 +30,6 @@ document.addEventListener("DOMContentLoaded",()=>{
         } else lbl.textContent="Raza *";
     });
 
-    // Foto validación
     document.getElementById("photo").addEventListener("change",e=>{
         const f=e.target.files[0];
         if(f&&(!['image/jpeg','image/png','image/webp'].includes(f.type)||f.size>5*1024*1024)){
